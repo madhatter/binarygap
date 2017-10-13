84df70ee1bb119e630f2271654c7d974cb1452c1
Arvid Warnecke <arvid.warnecke@otto.de>
2017-10-13 17:04:32 +0200

    Tests fixed.

--- binarygap_test.go
+++ binarygap_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func testfindGap(t *testing.T) {
+func TestFindGap(t *testing.T) {
 	tests := []struct {
 		num, expectedGap int
 	}{
@@ -15,6 +15,19 @@ func testfindGap(t *testing.T) {
 		{5, 1},
 		{16, 0},
 		{1024, 0},
+		{1162, 3},
+		{5, 1},
+		{51712, 2},
+		{20, 1},
+		{561892, 3},
+		{9, 2},
+		{66561, 9},
+		{6291457, 20},
+		{74901729, 4},
+		{805306373, 25},
+		{1376796946, 5},
+		{1073741825, 29},
+		{1610612737, 28},
 	}
 
 	for _, test := range tests {
